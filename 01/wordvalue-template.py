7c2ede0aa4339d4eb9c7df7f7245a69180fd5813
abi <abi.oluwade1@gmail.com>
2021-07-04 23:47:10 +0100

    completed load_words function

--- 01/wordvalue-template.py
+++ 01/wordvalue-template.py
@@ -2,6 +2,19 @@ from data import DICTIONARY, LETTER_SCORES
 
 def load_words():
     """Load dictionary into a list and return list"""
+    # Sets variable to dictionary.txt
+    dict_list = open(DICTIONARY)
+    # Sets a variable to an empty array to append list
+    x = []
+
+    # Simple for-loop logic, strip used to remove spaces at start and end of strings, then set variable
+    # to split the newly stripped line from txt file, finally append stripped and split line to empty an empty list
+    for line in dict_list:
+        stripped_line = line.strip()
+        line_list = stripped_line.split()
+        x.append(line_list)
+    dict_list.close()
+    print(x)
     pass
 
 def calc_word_value():
@@ -16,3 +29,5 @@ def max_word_value():
 
 if __name__ == "__main__":
     pass # run unittests to validate
+
+print(load_words())
